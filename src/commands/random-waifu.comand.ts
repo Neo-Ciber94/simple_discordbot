96e094fd831e9411e7824ed91d32ded989a566ea
Freddy L <neo-ciber_94@hotmail.com>
2022-06-26 02:30:21 -0400

    Updated bot

---
+++ src/commands/random-waifu.comand.ts
@@ -0,0 +1,44 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction, CacheType } from "discord.js";
+import fetch from "node-fetch";
+import { ICommand } from "./interfaces/ICommand";
+
+export enum Category {
+  SFW = "sfw",
+  NSFW = "nsfw",
+}
+
+type RandomWaifuResponse = {
+  url: string;
+};
+
+class RandomWaifuCommand implements ICommand {
+  readonly info = new SlashCommandBuilder()
+    .setName("waifu")
+    .addStringOption((option) =>
+      option
+        .setName("category")
+        .setDescription("The image category")
+        .setChoices(
+          { name: Category.SFW, value: Category.SFW },
+          { name: Category.NSFW, value: Category.NSFW }
+        )
+    )
+    .setDescription("Gets a random waifu image")
+    .toJSON();
+
+  async execute(interaction: CommandInteraction<CacheType>): Promise<void> {
+    const category = interaction.options.getString("category") || Category.SFW;
+    const res = await fetch(`https://api.waifu.pics/${category}/waifu`);
+
+    if (!res.ok) {
+      throw new Error(`${res.status} - ${res.statusText}`);
+    }
+
+    const json = (await res.json()) as RandomWaifuResponse;
+    const { url } = json;
+    await interaction.reply({ files: [url] });
+  }
+}
+
+export default new RandomWaifuCommand();
